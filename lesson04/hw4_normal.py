9b419f0937352732a12f47b6f6bc00146645557a
Anatoly Kalyuzhny <123588823+AnatolyKalyuzhny@users.noreply.github.com>
2023-05-22 21:06:44 +0400

    Update hw4_normal.py

--- lesson04/hw4_normal.py
+++ lesson04/hw4_normal.py
@@ -3,7 +3,18 @@
 # Первыми элементами ряда считать цифры 1 1
 
 def fibonacci(n, m):
-    pass
+    a, b = 1, 1
+    f_list = [1, ]
+
+    for i in range(m):
+        a, b = b, a + b
+        f_list.append(a)
+
+    return f_list[n - 1:m]
+
+
+print('fibonacci(4, 7): ', fibonacci(4, 7))
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -26,6 +37,16 @@ print(sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0]))
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
+def filter_list(fucnt, param_b):
+    end_list = list()
+    for x in param_b:
+        if fucnt(x) == True:
+            end_list.append(x)
+        else:
+            continue
+    return end_list
+
+print((filter_list((lambda x: x > 5), param_b=[3, 6, 50, 85, 58, 2, 100, 200, 40, 55, 60, 78, 8])))
 
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
